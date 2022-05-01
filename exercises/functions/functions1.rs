2dbb295432498d09ca8cb013384929fdc871dc6a
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2022-05-01 16:44:35 -0400

    start functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,6 +3,14 @@
 
 // I AM NOT DONE
 
+fn call_me()->u8 {
+    const age:u8 = 21;
+    const name:&str = "Victor";
+
+    println!("{} is {} years old.", name, age);
+    return age;
+}
+
 fn main() {
     call_me();
 }
